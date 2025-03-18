38f29f1c4f29ed97e4d25180fb18de9d27026461
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-03-18 17:18:47 -0600

    feat(componetns): uso de redux query

--- src/components/organisms/PostList.tsx
+++ src/components/organisms/PostList.tsx
@@ -1,52 +1,68 @@
-import { Link, Outlet } from "@tanstack/react-router"
-import { fetchPosts, type Post, selectPostAll, selectPostError, selectPostStatus } from "../../features/posts/postsSlice"
-import { useAppDispatch, useAppSelector } from "../../hooks"
-import { TimeAgo } from "../atoms/TimeAgo"
-import { ReactionButtons } from "../molecules/ReactionButtons"
-import { useEffect } from "react"
-import { Spiner } from "../atoms/Spiner"
-import { PostAuthor } from "../atoms/PostAuthor"
-
-interface PostProps {
-    post: Post
-}
-
-function Post ({ post }: PostProps) {
-    return (
-        <article className="outline-gray-700 rounded-md outline-1 w-100 flex flex-col items-start gap-2 px-4 py-2">
-            <PostAuthor userId={post.user} />
-            <TimeAgo timestamp={post.date} />
-            <h3 className="dark:text-white tracking-wide font-medium">
-                <Link to="/posts/$id" params={{ id: post.id }}>{post.title}</Link>
-            </h3>
-            <p className="post-name tracking-wide text-left">{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post} />
-        </article>
-    )
-}
+import { Outlet } from "@tanstack/react-router";
+//import {
+//    fetchPosts,
+//    selectPostAll,
+//    selectPostError,
+//    selectPostStatus,
+//} from "../../features/posts/postsSlice";
+import { useGetPostQuery } from '../../features/api/apiSlice'
+//import { useAppDispatch, useAppSelector } from "../../hooks";
+//import { useEffect } from "react";
+import { Spiner } from "../atoms/Spiner";
+import { PostExcerpt } from "../molecules/PostExcerpt";
 
 export function PostList () {
-    const dispatch = useAppDispatch()
+    const { data, isLoading, isError, isSuccess, error } = useGetPostQuery()
+    //const dispatch = useAppDispatch();
     // para leer los datos de la store usamos el hook useSelector
-    const posts = useAppSelector(selectPostAll) // Select the `state.posts` value from the store into the component
-    const postStatus = useAppSelector(selectPostStatus)
-    const postError = useAppSelector(selectPostError)
+    //const posts = useAppSelector(selectPostAll); // Select the `state.posts` value from the store into the component
+    //const postStatus = useAppSelector(selectPostStatus);
+    //const postError = useAppSelector(selectPostError);
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch])
+    //useEffect(() => {
+    //    if (postStatus === "idle") {
+    //        dispatch(fetchPosts());
+    //    }
+    //}, [postStatus, dispatch]);
 
-    let content: React.ReactNode
+    let content: React.ReactNode;
 
-    if (postStatus === 'pending') {
-        content = <Spiner />
-    } else if (postStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((postA, postB) => postB.date.localeCompare(postA.date))
-        content = <ul className="flex flex-col gap-4 w-fit">{orderedPosts.map(post => <li className="w-fit" key={`postId-${post.title}`}><Post post={post} /></li>)}</ul>
-    } else if (postStatus === 'rejected') {
-        content = <div>{postError}</div>
+    //if (postStatus === "pending") {
+    //    content = <Spiner />;
+    //} else if (postStatus === "succeeded") {
+    //    const orderedPosts = posts
+    //        .slice()
+    //        .sort((postA, postB) => postB.date.localeCompare(postA.date));
+    //    content = (
+    //        <ul className='flex flex-col gap-4 w-fit'>
+    //            {orderedPosts.map((post) => (
+    //                <li className='w-fit' key={`postId-${post.title}`}>
+    //                    <PostExcerpt post={post} />
+    //                </li>
+    //            ))}
+    //        </ul>
+    //    );
+    //} else if (postStatus === "rejected") {
+    //    content = <div>{postError}</div>;
+    //}
+
+    if (isLoading) {
+        content = <Spiner />;
+    } else if (isSuccess) {
+        const orderedPosts = data
+            .slice()
+            .sort((postA, postB) => postB.date.localeCompare(postA.date));
+        content = (
+            <ul className='flex flex-col gap-4 w-fit'>
+                {orderedPosts.map((post) => (
+                    <li className='w-fit' key={`postId-${post.title}`}>
+                        <PostExcerpt post={post} />
+                    </li>
+                ))}
+            </ul>
+        );
+    } else if (isError) {
+        content = <div>{error.toString()}</div>;
     }
 
     return (
@@ -54,5 +70,5 @@ export function PostList () {
             {content}
             <Outlet />
         </>
-    )
+    );
 }
